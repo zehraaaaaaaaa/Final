8ad80d1f14a3f29961002bc45fae0efbd04c7a15
zehraaaaaaaaa <92621349+zehraaaaaaaaa@users.noreply.github.com>
2021-11-30 13:59:42 +0500

    DONE

---
+++ ConsoleApplication1/ConsoleApplication1/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public interface Employee
+    {
+        HourlyEmployee HourlyEmployee { get; set; }
+        SalaryEmployee SalaryEmployee { get; set; }
+
+        void CalculateSalary();
+       
+        
+       
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/HourlyEmployee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class HourlyEmployee : Employee
+    {
+        public int EmployeeID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int EmployeeName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int EmployeeType
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int HoursWorkedInMonth
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int NumberOfUnitProduced
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
